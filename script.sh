b78e17d95dd5efd0bba51570cf6cfdd3f0003694
sergio.mendonca <stmfmendonca@rd.com.br>
2024-05-20 12:35:38 -0300

    script in shell linux update.

--- script.sh
+++ script.sh
@@ -3,15 +3,20 @@
 # Diretório onde o script está localizado
 script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Encontrar o arquivo que contém "_pos.xls" no nome
-file_to_keep=$(find "$script_directory" -type f -name "*_pos.xls" -print -quit)
+# Percorrer todas as pastas no diretório atual
+for folder in "$script_directory"/*/; do
+    # Verificar se é um diretório válido
+    if [ -d "$folder" ]; then
+        # Encontrar o arquivo que contém "_pos.xls" no nome
+        file_to_keep=$(find "$folder" -type f -name "*_pos.xls" -print -quit)
 
-# Verificar se foi encontrado um arquivo para manter
-if [ -n "$file_to_keep" ]; then
-    # Remover todos os arquivos, exceto o arquivo a ser mantido
-    find "$script_directory" -type f ! -name "$(basename "$file_to_keep")" -exec rm -f {} +
-
-    echo "Todos os arquivos, exceto '$file_to_keep', foram removidos da pasta '$script_directory'."
-else
-    echo "Nenhum arquivo com '_pos.xls' no nome encontrado na pasta '$script_directory'."
-fi
+        # Verificar se foi encontrado um arquivo para manter
+        if [ -n "$file_to_keep" ]; then
+            # Remover todos os arquivos, exceto o arquivo a ser mantido
+            find "$folder" -type f ! -name "$(basename "$file_to_keep")" -exec rm -f {} +
+            echo "Todos os arquivos, exceto '$file_to_keep', foram removidos da pasta '$folder'."
+        else
+            echo "Nenhum arquivo com '_pos.xls' no nome encontrado na pasta '$folder'."
+        fi
+    fi
+done
